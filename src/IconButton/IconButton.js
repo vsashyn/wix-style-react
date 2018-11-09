19dff6f1ed8137e426009db99bbefb402b1829d9
mykas <mykolass@wix.com>
2018-11-09 13:58:51 +0200

    Map props to core props and stylesheet classes

--- src/IconButton/IconButton.js
+++ src/IconButton/IconButton.js
@@ -2,18 +2,47 @@ import React from 'react';
 import {ButtonNext} from 'wix-ui-core/button-next';
 import classNames from 'classnames';
 import {iconButton, backofficeTheme} from 'wix-ui-core/themes/backoffice';
+import {string, node, bool} from 'prop-types';
+
 import WixComponent from '../BaseComponents/WixComponent';
 
 class IconButton extends WixComponent {
   static displayName = 'IconButton';
-  static propTypes = {};
-  static defaultProps = {};
+
+  static propTypes = {
+    skin: string,
+    priority: string,
+    size: string,
+    className: string,
+    children: node,
+    disabled: bool
+  };
+
+  static defaultProps = {
+    skin: 'standard',
+    priority: 'primary',
+    size: 'medium',
+    disabled: 'false'
+  };
 
   render() {
-    const {className, children, ...rest} = this.props;
+    const {
+      skin,
+      priority,
+      size,
+      className,
+      children,
+      disabled,
+      ...rest
+    } = this.props;
     return (
       <div className={backofficeTheme}>
-        <ButtonNext {...rest} className={classNames(className, iconButton())}>
+        <ButtonNext
+          {...rest}
+          data-hook="iconButton-core"
+          className={classNames(className, iconButton(skin, priority, size))}
+          disable={disabled}
+          >
           {children}
         </ButtonNext>
       </div>
